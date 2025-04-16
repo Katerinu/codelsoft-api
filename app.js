cb8d6f35e0e0a8aa0fc0d03d3d30764c860d6e39
Andres Prieto <katerinuuu@gmail.com>
2025-04-15 23:55:17 -0400

    feat: Enhance database configuration and error handling; add support for multiple MongoDB instances and global error middleware

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const authRouter = require('./src/modules/auth/routers/authRouter');
 const videoRouter = require('./src/modules/video/routers/videoRouter');
 const billsRouter = require('./src/modules/bills/routers/billsRouter');
 const usersRouter = require('./src/modules/users/routers/usersRouter');
+const globalErrorMiddleware = require('./src/middleware/globalErrorMiddleware');
 
 /*Configuracion de dotenv para poder acceder a las variables de entorno.*/
 config({path:'.env'});
@@ -17,13 +18,22 @@ const environment = process.env.NODE_ENV;
 
 /*Se reemplazan los valores en nuestro .env con la clave y el user correspondiente para asi conectarnos a nuestra DB de mongo.
 Se le esta asignando a uina variable para asi posteriormente conectarse por medio de mongoose*/
-const DB = process.env.MONGO_DATABASE.replace(
+const DB_VIDEOS = process.env.MONGO_DATABASE_VIDEOS.replace(
 "<PASSWORD>",
-process.env.MONGO_PASSWORD
-).replace("<USER>", process.env.MONGO_USER);
+process.env.MONGO_PASSWORD_VIDEOS
+).replace("<USER>", process.env.MONGO_USER_VIDEOS);
 
-connect(DB).then(() => console.log("✓ Conexión a base de datos exitosa"));
+connect(DB_VIDEOS).then(() => console.log("✓ Conexión a base de datos exitosa"));
 
+/*Se reemplazan los valores en nuestro .env con la clave y el user correspondiente para asi conectarnos a nuestra DB de mongo.
+Se le esta asignando a uina variable para asi posteriormente conectarse por medio de mongoose*/
+const DB_USERS = process.env.MONGO_DATABASE_USERS.replace(
+    "<PASSWORD>",
+    process.env.MONGO_PASSWORD_USERS
+    ).replace("<USER>", process.env.MONGO_USER_USERS);
+    
+    connect(DB_USERS).then(() => console.log("✓ Conexión a base de datos exitosa"));
+    
 
 /*Configuracion de express para el funcionamiento de la API.*/
 /*Se añade morgan para visualizar el uso de la API y .json para transformar todo a JSON*/
@@ -31,16 +41,18 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-/*Ruta base*/
-app.get('/', (req, res) => {
-    res.status(200).send("OK");
-});
-
 /*Configuracion de los routers para poder acceder a las rutas correspondientes. Se les agrega a nuestra app.*/
 app.use("/auth", authRouter);
 app.use("/videos", videoRouter);
 app.use("/bills", billsRouter);
 app.use("/users", usersRouter);
+
+/*Ruta base*/
+app.get('/', (req, res) => {
+    res.status(200).send("OK");
+});
+
+app.use(globalErrorMiddleware);
 
 /*Configuracion de la API para que escuche en el puerto correspondiente y se le asigna el entorno.
 Esto ademas sirve para ver el estado de la API en la consola.*/
